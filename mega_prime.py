7b332b1b6e7338de8e19ab079252d72d0db3644e
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-05-31 09:46:06 +0530

    n=int(input()) t=n c=0 m=0 s=0 for i in range(1,t+1,1):     if t%i==0:         c=c+1 if c==2:     while n:         r=n%10         m=m+1         n=n//10         k=0         for i in range(1,r+1,1):             if r%i==0:                 k=k+1         if k==2:             s=s+1 if c==2 and s==m:     print("Mega Prime") elif c>2 or s!=m:     print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,16 +1,17 @@
 n=int(input())
+t=n
 c=0
 m=0
 s=0
-for i in range(1,n+1):
-    if n%i==0:
+for i in range(1,t+1,1):
+    if t%i==0:
         c=c+1
 if c==2:
     while n:
-        k=0
         r=n%10
         m=m+1
         n=n//10
+        k=0
         for i in range(1,r+1,1):
             if r%i==0:
                 k=k+1
