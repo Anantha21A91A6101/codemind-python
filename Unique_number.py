59fc33cff721a3438e8d9d7f53843bd33c48ec1d
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-06-30 10:31:31 +0530

    n=int(input()) k=list() c=0 while n>0:     r=n%10     k.append(r)     n=n//10 for i in k:     if k.count(i)>1:         c=c+1 if c==0:     print("Unique Number") else:     print("Not Unique Number");

--- Unique_number.py
+++ Unique_number.py
@@ -1,14 +1,14 @@
 n=int(input())
-l=list()
-m=0
+k=list()
+c=0
 while n>0:
     r=n%10
-    l.append(r)
+    k.append(r)
     n=n//10
-for i in l:
-    if l.count(i)>1:
-        m=m+1
-if m==0:
-    print('Unique Number')
+for i in k:
+    if k.count(i)>1:
+        c=c+1
+if c==0:
+    print("Unique Number")
 else:
-    print('Not Unique Number')
+    print("Not Unique Number");
