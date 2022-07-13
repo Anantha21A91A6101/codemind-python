2e59f501cf798e466784439901501e49286d6e75
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-07-13 11:33:33 +0530

    n=int(input()) a=list(map(int,input().split())) m=0 c=0 for i in range(0,len(a)):     c=0     for j in range(0,len(a)):         if a[i]==a[j] and a[i]!=-1:             c=c+1     if c==a[i]:         m=m+1         a[i]=-1 print(m,end="")

---
+++ value_same_as_count.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+m=0
+c=0
+for i in range(0,len(a)):
+    c=0
+    for j in range(0,len(a)):
+        if a[i]==a[j] and a[i]!=-1:
+            c=c+1
+    if c==a[i]:
+        m=m+1
+        a[i]=-1
+print(m,end="")        
