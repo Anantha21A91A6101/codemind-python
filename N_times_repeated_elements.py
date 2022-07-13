53c7f240d8cf446cfe6aaf401458fdfeea5c9a13
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-07-13 11:39:13 +0530

    n=int(input()) a=list(map(int,input().split())) k=int(input()) m=0 for i in range(0,len(a)):     c=0     for j in range(0,len(a)):         if a[i]==a[j] and a[i]!=-1:             c=c+1     if c==k:         print(a[i],end=" ")         a[i]=-1         m=m+1 if m==0:     print("-1");

---
+++ N_times_repeated_elements.py
@@ -0,0 +1,17 @@
+n=int(input())
+a=list(map(int,input().split()))
+k=int(input())
+m=0
+for i in range(0,len(a)):
+    c=0
+    for j in range(0,len(a)):
+        if a[i]==a[j] and a[i]!=-1:
+            c=c+1
+    if c==k:
+        print(a[i],end=" ")
+        a[i]=-1
+        m=m+1
+if m==0:
+    print("-1");
+        
+        
