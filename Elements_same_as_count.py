ed3ff755aa548c102ec1da6be333178b084a68f4
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-07-13 11:36:30 +0530

    n=int(input()) a=list(map(int,input().split())) m=0 for i in range(0,len(a)):     c=0     for j in range(0,len(a)):         if a[i]==a[j] and a[i]!=-1:             c=c+1     if c==a[i]:         print(a[i],end=" ")         a[i]=-1         m=m+1 if m==0:     print("-1");

---
+++ Elements_same_as_count.py
@@ -0,0 +1,16 @@
+n=int(input())
+a=list(map(int,input().split()))
+m=0
+for i in range(0,len(a)):
+    c=0
+    for j in range(0,len(a)):
+        if a[i]==a[j] and a[i]!=-1:
+            c=c+1
+    if c==a[i]:
+        print(a[i],end=" ")
+        a[i]=-1
+        m=m+1
+if m==0:
+    print("-1");
+        
+        
