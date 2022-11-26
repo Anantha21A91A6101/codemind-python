6fc8718a4e096321770464f2bb0321583be9c2c5
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-11-26 10:05:30 +0530

    n=int(input()) a=list(map(int,input().split())) c=[] d=[] for i in range(0,len(a)):     if a[i]%2==0:         c.append(a[i])     else:         d.append(a[i]) i=0 j=0 while i<len(c) or j<len(d):     if i<len(c):         print(c[i],end=" ")         i=i+1     if j<len(d):         print(d[j],end=" ")         j=j+1 if n%2!=0:     print("0")

---
+++ even_odd_pairs.py
@@ -0,0 +1,20 @@
+n=int(input())
+a=list(map(int,input().split()))
+c=[]
+d=[]
+for i in range(0,len(a)):
+    if a[i]%2==0:
+        c.append(a[i])
+    else:
+        d.append(a[i])
+i=0
+j=0
+while i<len(c) or j<len(d):
+    if i<len(c):
+        print(c[i],end=" ")
+        i=i+1
+    if j<len(d):
+        print(d[j],end=" ")
+        j=j+1
+if n%2!=0:
+    print("0")
