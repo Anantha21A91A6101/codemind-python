41cc3c2800a9f5c531eb8461652df99e4df15397
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-12-01 21:53:43 +0530

    n=input() n=n.lower() n=n.split() n="".join(n) m=0 c=0 for i in n:     c=0     for j in n:         if i==j:             c=c+1     if c==1:         m=m+1 print(m)

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,17 +1,14 @@
 n=input()
 n=n.lower()
 n=n.split()
-c=[]
+n="".join(n)
 m=0
-s=0
+c=0
 for i in n:
-    for j in i:
-        c.append(j)
-for i in range(0,len(c)):
-    m=0
-    for j in range(0,len(c)):
-        if c[i]==c[j]:
-            m=m+1
-    if m==1:
-        s=s+1
-print(s)
+    c=0
+    for j in n:
+        if i==j:
+            c=c+1
+    if c==1:
+        m=m+1
+print(m)
