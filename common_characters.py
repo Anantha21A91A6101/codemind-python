81f1f56d9a1fc9f2b2a265f123df7b664489c47f
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-12-01 21:43:40 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() s1=s1.split() s2=s2.split() s1="".join(s1) s2="".join(s2) c=[] m=0 for i in s1:     for j in s2:         if i==j:             c.append(j)             m=1 if m==0:     print("-1") else:     from collections import OrderedDict     k=sorted(c)     k="".join(OrderedDict.fromkeys(k))     for i in k:         print(i,end="")

--- common_characters.py
+++ common_characters.py
@@ -8,10 +8,10 @@ s1="".join(s1)
 s2="".join(s2)
 c=[]
 m=0
-for i in range(0,len(s1)):
-    for j in range(0,len(s2)):
-        if s1[i]==s2[j]:
-            c.append(s1[i])
+for i in s1:
+    for j in s2:
+        if i==j:
+            c.append(j)
             m=1
 if m==0:
     print("-1")
