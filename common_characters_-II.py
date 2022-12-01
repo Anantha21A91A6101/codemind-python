87834ec2321dafcd542faef471118b2f58a3ab98
Anantha21A91A6101 <98092218+Anantha21A91A6101@users.noreply.github.com>
2022-12-01 21:29:07 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() s1=s1.split() s2=s2.split() s1="".join(s1) s2="".join(s2) c=[] m=0 for i in s1:     for j in s2:         if i==j:             c.append(i) from collections import OrderedDict k=sorted(c) k="".join(OrderedDict.fromkeys(k)) print(len(k))

---
+++ common_characters_-II.py
@@ -0,0 +1,18 @@
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+s1=s1.split()
+s2=s2.split()
+s1="".join(s1)
+s2="".join(s2)
+c=[]
+m=0
+for i in s1:
+    for j in s2:
+        if i==j:
+            c.append(i)
+from collections import OrderedDict
+k=sorted(c)
+k="".join(OrderedDict.fromkeys(k))
+print(len(k))
